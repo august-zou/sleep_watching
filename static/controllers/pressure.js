46bc97ad4c05e28f1a5061aa1b03f15819e50ff8
august-zou <zjzouxu@gmail.com>
2014-12-16 12:45:32 +0800

    commit

--- static/controllers/pressure.js
+++ static/controllers/pressure.js
@@ -1,16 +1,16 @@
 angular.module('sleepWatchingApp').controller('PressureCtrl',function($scope,socket) {
-  var TOTAL_DATA_LENGTH = 240;
+  var TOTAL_DATA_LENGTH = 6000;
   $scope.data = new Array(TOTAL_DATA_LENGTH);
   for(var i=0;i<$scope.data.length;++i)$scope.data[i] = 0;
   socket.on('pressure.read',function(data) {
-   $scope.data = data.concat($scope.chartConfig.series[0].data.slice(0,179));
+   $scope.data = data.concat($scope.chartConfig.series[0].data.slice(0,TOTAL_DATA_LENGTH-data.length-1));
 
    $scope.chartConfig.series[0].data = $scope.data;
    //var series =  $scope.chartConfig.series[0];
    //series.addPoint(data,true,true,true);
   });
   $scope.chartSeries = [
-    {"name": "Some data", "data": $scope.data},
+    {"name": "pressure data", "data": $scope.data},
    ];
 
   $scope.addPoints = function () {
@@ -22,7 +22,9 @@ angular.module('sleepWatchingApp').controller('PressureCtrl',function($scope,soc
   $scope.chartConfig = {
     options: {
       chart: {
-        type: 'spline'
+        type: 'spline',
+        animation:false
+
       },
       plotOptions: {
         series: {
@@ -30,12 +32,19 @@ angular.module('sleepWatchingApp').controller('PressureCtrl',function($scope,soc
           animation:false
         }
       },
-      animation:false
     },
     series: $scope.chartSeries,
     yAxis:{
       max: 255,
       min:0
+    },
+    xAxis:{
+      tickInterval: 240,
+      labels: {
+        formatter: function () {
+          return this.value / 60 + 's';
+          }
+         } 
     },
     title: {
       text: '压力变化'
@@ -47,7 +56,4 @@ angular.module('sleepWatchingApp').controller('PressureCtrl',function($scope,soc
     size: {}
   };
 
-  $scope.reflow = function () {
-    $scope.$broadcast('highchartsng.reflow');
-  };  
 });
